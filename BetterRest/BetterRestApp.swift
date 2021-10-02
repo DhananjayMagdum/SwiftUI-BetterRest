99d4e2879b921399e1e3ed4476068c94cdcaf8b5
Dhananjay Magdum <dhananjay.magdum@atlascopco.com>
2021-10-02 06:14:14 +0530

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Dhananjay Magdum on 02/10/21.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
